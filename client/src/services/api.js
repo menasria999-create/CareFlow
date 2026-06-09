10412a68bbdf5d66251e0f1267588ee7f5afbd90
menasria999-create <menasria999@gmail.com>
2026-06-10 11:43:38 +1200

    Update API_URL to Railway backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://careflow-production-3da8.up.railway.app/api';
 
 const api = axios.create({
     baseURL: API_URL,
