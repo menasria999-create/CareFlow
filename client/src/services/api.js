c5717115a5c4cbfd215c16dd3062cd36fd72e0b4
menasria999-create <menasria999@gmail.com>
2026-06-10 20:35:05 +1200

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 // ✅ الآن نستخدم المتغير الذي تم تمريره من vite.config.js
-// إذا لم ينجح، سيستخدم الرابط المحلي كحل احتياطي
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
